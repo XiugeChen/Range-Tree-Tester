7f52fb2c3d96168a3a0c799c6fd7ce60495653e9
Xiuge Chen <xiugec@student.unimelb.edu.au>
2020-05-21 15:50:57 +1000

    add external utils such as spdlog and likely/unlikely macros

---
+++ include/utils.h
@@ -0,0 +1,11 @@
+//
+// Created by Xiuge Chen on 5/21/20.
+//
+
+#ifndef RANGETREE_UTILS_H
+#define RANGETREE_UTILS_H
+
+#define likely(x)    __builtin_expect(!!(x), 1)
+#define unlikely(x)  __builtin_expect(!!(x), 0)
+
+#endif //RANGETREE_UTILS_H
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <spdlog/spdlog.h>
+
+int main() {
+    spdlog::set_level(spdlog::level::info);
+    spdlog::info("Start range tree tester");
+
+
+
+    return 0;
+}
